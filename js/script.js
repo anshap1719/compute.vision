70d23be1734f02b62cf8646f89654584fb5c00ac
Anshul Sanghi <anshap1719@gmail.com>
2018-02-14 16:35:28 +0530

    Fix Background, Add Functionality To Try More Buttons

--- js/script.js
+++ js/script.js
@@ -14,3 +14,17 @@
 //     bindings.colorResults(urls.slice(20, 39));
 //     bindings.queryImage(queryImage);
 // };
+
+const imageUpload = () => {
+    window.scroll({top: 0, left: 0, behavior: 'smooth' });
+    const event = new Event('click');
+    const el = document.getElementById('image');
+    el.dispatchEvent(event);
+};
+
+const similarLogo = () => {
+    window.scroll({top: 0, left: 0, behavior: 'smooth' });
+    const event = new Event('click');
+    const el = document.getElementById('url');
+    el.dispatchEvent(event);
+};
